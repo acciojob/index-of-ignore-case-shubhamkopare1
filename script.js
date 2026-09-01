31df91c3a61130c3105d2fbcb3a061e872a20c0b
Shubham Kopare <149860270+shubhamkopare1@users.noreply.github.com>
2026-09-01 15:01:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  if (s1 === null || s2 === null) return -1;
+  
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
